bce90c868ed37446ac903841065863f69f51a30c
ashabanov <me.antony.rain@gmail.com>
2014-08-23 23:11:10 +0400

    [ADDED]: added module pattern example.

---
+++ static/js/modulePattern.js
@@ -0,0 +1,27 @@
+/*
+* This is example of Module patter on native js: realization of user cart
+* */
+
+var userCart = (function(){
+    var cartItems = [];
+
+    return {
+        addItem: function(item) {
+            cartItems.push(item);
+            return this;
+        },
+        getItemsCount: function() {
+            return cartItems.length;
+        },
+        getTotalPrice: function() {
+            var count = this.getItemsCount(),
+                price = 0;
+
+            while (count--) {
+                price += cartItems[count].price;
+            }
+
+            return price;
+        }
+    };
+}());
